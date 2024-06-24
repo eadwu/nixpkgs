9703955a2d372059fba75a89c18e3fbb31c867e2
Edmund Wu <fangkazuto@gmail.com>
2024-06-24 01:02:25 -0400

    vscode-extensions.ms-toolsai.jupyter: 2024.5.0 -> 2024.6.2024062001

--- pkgs/applications/editors/vscode/extensions/ms-toolsai.jupyter/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-toolsai.jupyter/default.nix
@@ -9,8 +9,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "jupyter";
     publisher = "ms-toolsai";
-    version = "2024.5.0";
-    hash = "sha256-bGHXbqv+YXC8NUXIY+bxFsMvoV6h8E2/2F6Ku4bJwT0=";
+    version = "2024.6.2024062001";
+    sha256 = "1gkarlmr91m9g9azmv01kwhly82ydp2k8d7y8iw0r59nhc7vhs0b";
   };
 
   nativeBuildInputs = [
